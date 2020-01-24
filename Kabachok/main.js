7432702a3b6117e64506b35c4bc01be08f6dc970
Семен Вяцков <43330319+susimus@users.noreply.github.com>
2020-01-24 16:38:40 +0500

    The job is done

--- Kabachok/main.js
+++ Kabachok/main.js
@@ -52,28 +52,6 @@ function getDates() {
     return dates;
 }
 
-function tableIsAccessible(
-        table_number, datetime, time_start, time_end) {
-    var result = true;
-
-    database.all(
-        `SELECT * FROM Schedule
-            WHERE TableNumber = ? 
-                AND Datetime = ?`,
-        [table_number, datetime],
-        (err, rows) => {
-            rows.forEach(
-                (row) => {
-                    if (row.TimeEnd > time_start 
-                            & row.TimeStart < time_end)
-                        result = false;
-                })
-            
-        });
-
-    return result;
-}
-
 function addScheduleEntry(
         table_number, 
         name, 
@@ -119,30 +97,82 @@ app.use(
 
         if (req.method == "POST" 
                 & req.body.query_type == "access")
-            if (req.body.time_start >= req.body.time_end)
+            if (req.body.time_start >= req.body.time_end) {
                 access_str = 
                     "Время было указанно некорректно. Время начала было больше либо равно времени конца.";
+
+                res.render(
+                    'index.hbs',
+                    { 
+                        select_time: 
+                            ['12:00', '13:00', '14:00', '15:00', '16:00', 
+                                '17:00', '18:00', '19:00', '20:00', '21:00'],
+                        tables: [1, 2, 3, 4, 5, 6],
+                        dates: getDates(),
+                        access_result: access_str,
+                        reservation_result: reservation_str
+                    });
+            }
             
-            else if (
-                    !tableIsAccessible(
-                        parseInt(req.body.table),
-                        req.body.date,
-                        req.body.time_start,
-                        req.body.time_end))
-                access_str = 
-                    "Столик номер" + req.body.table
-                    + " уже забронирован " + req.body.date 
-                    + " числа между " + req.body.time_start
-                    + " и " + req.body.time_end;
+            else {
+                function renderAccessMessage(err, rows) {
+                    let table_is_accessible = true;
 
-            else
-                access_str = 
-                    "Столик номер " + req.body.table
-                    + " свободен для бронирования " 
-                    + req.body.date + " числа между " 
-                    + req.body.time_start + " и " 
-                    + req.body.time_end;
+                    for (let i = 0; i < rows.length; i++)
+                        if (rows[i].TimeEnd 
+                                > req.body.time_start
+                                & rows[i].TimeStart 
+                                < req.body.time_end)
+                            table_is_accessible = false;
 
+                    if (!table_is_accessible)
+                        access_str = 
+                            "Столик номер " 
+                            + req.body.table
+                            + " уже забронирован " 
+                            + req.body.date 
+                            + " числа между " 
+                            + req.body.time_start
+                            + " и " + req.body.time_end;
+                    else
+                        access_str = 
+                            "Столик номер " 
+                            + req.body.table
+                            + " свободен для бронирования " 
+                            + req.body.date 
+                            + " числа между " 
+                            + req.body.time_start 
+                            + " и " 
+                            + req.body.time_end;
+
+                    res.render(
+                        'index.hbs',
+                        { 
+                            select_time: 
+                                ['12:00', 
+                                 '13:00', 
+                                 '14:00', 
+                                 '15:00', 
+                                 '16:00', 
+                                 '17:00', 
+                                 '18:00', 
+                                 '19:00',  
+                                 '20:00', 
+                                 '21:00'],
+                            tables: [1, 2, 3, 4, 5, 6],
+                            dates: getDates(),
+                            access_result: access_str,
+                            reservation_result: reservation_str
+                        });
+                }
+
+                database.all(
+                    `SELECT * FROM Schedule
+                        WHERE TableNumber = ? 
+                            AND Datetime = ?`,
+                    [parseInt(req.body.table), req.body.date],
+                    renderAccessMessage);
+            }
         else if (req.method == "POST" 
                 & req.body.query_type == "reservation")
             if (!req.body.hasOwnProperty("table1")
@@ -150,74 +180,148 @@ app.use(
                     & !req.body.hasOwnProperty("table3")
                     & !req.body.hasOwnProperty("table4")
                     & !req.body.hasOwnProperty("table5")
-                    & !req.body.hasOwnProperty("table6"))
+                    & !req.body.hasOwnProperty("table6")) {
                 reservation_str = "Ни один стол не был выбран";
+
+                res.render(
+                    'index.hbs',
+                    { 
+                        select_time: 
+                            ['12:00', '13:00', '14:00', '15:00', '16:00', 
+                                '17:00', '18:00', '19:00', '20:00', '21:00'],
+                        tables: [1, 2, 3, 4, 5, 6],
+                        dates: getDates(),
+                        access_result: access_str,
+                        reservation_result: reservation_str
+                    });
+            }
             
-            else if (req.body.time_start >= req.body.time_end)
+            else if (req.body.time_start >= req.body.time_end) {
                 reservation_str = 
                     "Время было указанно некорректно. Время начала было больше либо равно времени конца.";
+
+                res.render(
+                    'index.hbs',
+                    { 
+                        select_time: 
+                            ['12:00', '13:00', '14:00', '15:00', '16:00', 
+                                '17:00', '18:00', '19:00', '20:00', '21:00'],
+                        tables: [1, 2, 3, 4, 5, 6],
+                        dates: getDates(),
+                        access_result: access_str,
+                        reservation_result: reservation_str
+                    });
+            }
             
             else {
                 let tables_numbers = [];
+                let tables_names = 
+                    ["table1", 
+                     "table2", 
+                     "table3",
+                     "table4", 
+                     "table5", 
+                     "table6"];
+                let tablesDisjunction = "(TableNumber = ?";
                 
-                ["table1", "table2", "table3",
-                 "table4", "table5", "table6"]
-                    .forEach((table_name) => {
-                        if (req.body.hasOwnProperty(table_name))
-                            tables_numbers.push(
-                                parseInt(table_name[5]));
-                    });
-                
-                let tablesAreAccessible = true;
-                
-                for (let i = 0; i < tables_numbers.length; i++)
-                    if (!tableIsAccessible(
-                            tables_numbers[i],
-                            req.body.date,
-                            req.body.time_start,
-                            req.body.time_end))
-                        tablesAreAccessible = false;
-                
-                if (!tablesAreAccessible)
-                    reservation_str =
-                        "Стол(ы) " + tables_numbers.toString()
-                        + " уже забронированы " + req.body.date 
-                        + " числа между " + req.body.time_start
-                        + " и " + req.body.time_end;
-                else {
-                    for (
-                            let i = 0; 
-                            i < tables_numbers.length; 
-                            i++)
-                        addScheduleEntry(
-                            tables_numbers[i],
-                            req.body.name, 
-                            req.body.phone,
-                            req.body.email,
-                            req.body.date,
-                            req.body.time_start,
-                            req.body.time_end);
+                for (let table_name of tables_names)
+                    if (req.body.hasOwnProperty(table_name)) {
+                        tables_numbers.push(
+                            parseInt(table_name[5]));
 
-                    reservation_str =
-                        "Стол(ы) " + tables_numbers.toString()
-                        + " были успешно забронированы на " 
-                        + req.body.date + " число между " 
-                        + req.body.time_start + " и " 
-                        + req.body.time_end;
+                        if (tables_numbers.length > 1)
+                            tablesDisjunction += 
+                                " OR TableNumber = ?";
+                    }
+
+                tablesDisjunction += ")";
+
+                function renderReservationMessage(err, rows) {
+                    let tables_are_accessible = true;
+                    
+                    for (let i = 0; i < rows.length; i++)
+                        if (rows[i].TimeEnd 
+                                > req.body.time_start
+                                & rows[i].TimeStart 
+                                < req.body.time_end)
+                            tables_are_accessible = false;
+                    
+                    if (!tables_are_accessible)
+                        reservation_str =
+                            "Стол(ы) " 
+                            + tables_numbers.toString()
+                            + " уже забронированы " 
+                            + req.body.date 
+                            + " числа между " 
+                            + req.body.time_start
+                            + " и " + req.body.time_end;
+                    else {
+                        for (
+                                let i = 0; 
+                                i < tables_numbers.length; 
+                                i++)
+                            addScheduleEntry(
+                                tables_numbers[i],
+                                req.body.name, 
+                                req.body.phone,
+                                req.body.email,
+                                req.body.date,
+                                req.body.time_start,
+                                req.body.time_end);
+
+                        reservation_str =
+                            "Стол(ы) " 
+                            + tables_numbers.toString()
+                            + " были успешно забронированы на " 
+                            + req.body.date 
+                            + " число между " 
+                            + req.body.time_start 
+                            + " и " 
+                            + req.body.time_end;
+                    }
+
+                    res.render(
+                        'index.hbs',
+                        { 
+                            select_time: 
+                                ['12:00', 
+                                 '13:00', 
+                                 '14:00', 
+                                 '15:00', 
+                                 '16:00', 
+                                 '17:00', 
+                                 '18:00', 
+                                 '19:00',  
+                                 '20:00', 
+                                 '21:00'],
+                            tables: [1, 2, 3, 4, 5, 6],
+                            dates: getDates(),
+                            access_result: access_str,
+                            reservation_result: reservation_str
+                        });
                 }
+                
+                database.all(
+                    "SELECT * FROM Schedule "
+                        + "WHERE " 
+                            + tablesDisjunction
+                            + "AND Datetime = ?",
+                    tables_numbers.concat([req.body.date]),
+                    renderReservationMessage);
             }
-
-        res.render(
-            'index.hbs',
-            { 
-                select_time: 
-                    ['12:00', '13:00', '14:00', '15:00', '16:00', 
-                     '17:00', '18:00', '19:00', '20:00', '21:00'],
-                tables: [1, 2, 3, 4, 5, 6],
-                dates: getDates(),
-                access_result: access_str,
-                reservation_result: reservation_str
-            });
+        else
+            res.render(
+                'index.hbs',
+                { 
+                    select_time: 
+                        ['12:00', '13:00', '14:00', '15:00',
+                         '16:00', '17:00', '18:00', '19:00', 
+                         '20:00', '21:00'],
+                    tables: [1, 2, 3, 4, 5, 6],
+                    dates: getDates(),
+                    access_result: access_str,
+                    reservation_result: reservation_str
+                });
     })
 
 app.listen(3000)
